9549f3e1ac4d1027732c5ab7fe216aad14ccb2f6
liuhaiqing <531589768@qq.com>
2018-08-26 14:35:53 +0800

    【vueDemo】 head-bar、mcontent、foot-bar公共组件实现；首页展示实现。

---
+++ demos/vue-demo-20180814/src/components/mcontent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="content" class="content">
+    <img src="../assets/logo.png"/>
+    <div class="content">
+      {{mcontent}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      mcontent: String,
+      required: true,
+      default: '默认内容'
+  }
+}
+</script>
+
+<style>
+  .content {
+    width: 100%;
+    text-align: center;
+    margin-top:60px;
+  }
+</style>
--- demos/vue-demo-20180814/src/page/mainPage.vue
+++ demos/vue-demo-20180814/src/page/mainPage.vue
@@ -1,3 +1,67 @@
 <template>
-    <div>登录成功</div>
+    <div>
+      <head-bar v-bind:title="pageone.title" class="head"></head-bar>
+      <mcontent v-bind:mcontent="pageone.mcontent" class="mcontent"></mcontent>
+      <foot-bar class="foot"></foot-bar>
+    </div>
 </template>
+
+<script>
+// 导入组件
+import mcontent from '../components/mcontent.vue'
+import headBar from '../components/head-bar.vue'
+import footBar from '../components/foot-bar.vue'
+export default {
+  components: {
+    mcontent, headBar, footBar
+  },
+  data () {
+    return {
+      pageone: {
+        title: '首页',
+        mcontent: '我是首页'
+      },
+      pagetwo: {
+        title: '发现',
+        mcontent: ' 我是发现'
+      },
+      pagethree: {
+        title: '我的',
+        mcontent: ' 我是我的我'
+      },
+      pagefour: {
+        title: '设置',
+        mcontent: ' 我是设置'
+      }
+    //   title: this.$route.query.title
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #fbfaf7;
+}
+
+.head {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.mcontent {
+  height: 100%;
+  width: 100%;
+}
+
+.foot {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left:0;
+}
+</style>
