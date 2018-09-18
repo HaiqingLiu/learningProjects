fc12e22e1f0a05b55832518ef2036b833ec20b4a
liuhaiqing <liuhaiqing@yunzhangfang.com>
2018-09-18 19:12:47 +0800

    【vue-drag-echarts】项目初始化。

---
+++ demos/vue-drag-echarts/src/components/DragEcharts.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <div>
+        <button>保存</button>
+        <button>预览</button>
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
