55006efc94aaec7094a2f130a0c4330b6b0759cd
liuhaiqing <531589768@qq.com>
2018-09-24 16:44:37 +0800

    【angular-dragEcharts】画布设置区域功能实现。

---
+++ AngularProject/angular-dragEcharts/src/app/main/bean/area.class.ts
@@ -0,0 +1,6 @@
+export interface Area {
+    width: string,
+    height: string,
+    backgroundColor: string,
+    backgroundImg: any
+}
--- AngularProject/angular-dragEcharts/src/app/main/main.component.ts
+++ AngularProject/angular-dragEcharts/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Area } from './bean/area.class';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  @ViewChild("file1") fileUpload;
+  private areaDefault: Area = {
+      width: "500px",
+      height: "500px",
+      backgroundColor: "#ffffff",
+      backgroundImg: null
+  }
+  area: Area = Object.assign({}, this.areaDefault);
+  areaStyle;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  imgSelect($event) {
+    let fileEle = this.fileUpload.nativeElement;
+    let files = fileEle.files;
+    if (FileReader && files && files.length) {
+      var fr = new FileReader();
+      fr.onload = () => {
+        this.area.backgroundImg = fr.result;
+      }
+      fr.readAsDataURL(files[0]);
+    }
+  }
+
+  clearImg() {
+    this.area.backgroundImg = null;
+  }
+
+  areaChange() {
+    // console.log(this.area);
+    this.reDisply();
+  }
+
+  private reDisply() {
+    let img = `url(${this.area.backgroundImg})`;
+    // console.log(img);
+    this.areaStyle = {
+      width: this.area.width,
+      height: this.area.height,
+      'background-color': this.area.backgroundColor,
+      'background-image': img
+    }
+
+  }
+
+  areaReset() {
+    this.area = Object.assign({}, this.areaDefault);
+    this.reDisply();
+  }
 }
